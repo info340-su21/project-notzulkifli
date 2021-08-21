71899f98f77d8dfbd1b4781fc61d02c7deaf50c4
jboyle99 <62917214+jboyle99@users.noreply.github.com>
2021-08-20 22:52:56 -0700

    added favorite feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { HeadMetadata } from './HeadMetaData';
 import { Navigation } from './Navigation';
 import { Footer } from './Footer';
 
 export default function App() {
+    const [favoriteCard, setExerciseBookmark] = useState([]);
+
+    const handleExerciseBookmark = (card) => {
+        favoriteCard.push(card);
+        let updatedBookmarkExerciseCards = favoriteCard;
+        setExerciseBookmark(updatedBookmarkExerciseCards)
+    }
+
     return (
         <div>
             < HeadMetadata />
-            < Navigation />
+            < Navigation handleExerciseBookmark={handleExerciseBookmark} favoriteCard={favoriteCard}/>
             < Footer />
         </div>
     )
