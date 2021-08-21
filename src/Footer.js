f019ea3daadbf19aa8f07ea2d8bf0b5ae396d583
Zulkifli Sales <zulkiflisales@gmail.com>
2021-08-21 00:51:11 -0700

    Final website deployed

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ export function Footer() {
         <footer>
             <p>&copy; Group 13: John Boyle, Shriyansh Sharma, and Zulkifli Sales of className INFO 340</p>
             <p>Undergraduate students at the<a href="https://www.washington.edu/"> University of Washington (Seattle)</a></p>
+            <p>Data from <a href="https://gist.github.com/JakubPetriska/060958fd744ca34f099e947cd080b540">Jakub Petriska</a></p>
             <address>Contact us at:
                 <a href="mailto:zsales@uw.edu" className="contact">zsales@uw.edu</a>
                 <h3>Follow our social media pages:</h3>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import firebase from 'firebase/app';
 
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBBYuHRWmcYunbq9UjGT3d2Z-mBFOSx5c4",
+  authDomain: "mobile-nutrition1.firebaseapp.com",
+  projectId: "mobile-nutrition1",
+  storageBucket: "mobile-nutrition1.appspot.com",
+  messagingSenderId: "337831355056",
+  appId: "1:337831355056:web:1c32781606baf3f6d99175"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
